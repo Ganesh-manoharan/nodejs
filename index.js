6062afac55ddea7cd6d6f64bc5d488fcbd6a63ee
Ganesh-manoharan <78746251+Ganesh-manoharan@users.noreply.github.com>
2021-02-08 22:08:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Heroku CICD Test - AIT!')
+  response.send('Heroku CICD Test version 1!')
 })
 
 app.listen(app.get('port'), function() {
